f104e5ef77f44e5ac5aecd67e324741d0ced708c
b1621 <bewuket1621@gmail.com>
2023-07-29 16:25:28 +0300

    loading questions from a local json server api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from "react";
-import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+
+  // 'loadin','error','ready','active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unkown");
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <>
       <div className=" absolute top-0  h-screen w-screen bg-teal-800 text-white">
-        <DateCounter />
+        <Header />
+        <Main>
+          <p>1/15</p>
+          <p>Questions?</p>
+        </Main>
       </div>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Header = () => {
+  return (
+    <header className=" border mx-auto w-fit text-6xl ">
+      <h1>The React Quiz</h1>
+    </header>
+  );
+};
+
+export default Header;
